93957e511a3c0f967315e2c9dc0a623cd90ff8ff
GraziellyRaissa1 <grazyraissa7@gmail.com>
2024-05-20 01:26:00 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+const [numero, setNumero] = useState(0)
+
+function aumentar(){
+  setNumero(numero + 1)
 }
 
-export default App;
+function diminuir(){
+  setNumero(numero - 1)
+}
+
+  return <section>
+    <h1>Contadora</h1>
+    <p>{numero}</p>
+    <div id="buttons">
+      <button onClick={aumentar}>Mais</button>
+    <button onClick={diminuir}>Menos</button>
+    </div>
+    
+  </section>
+}
